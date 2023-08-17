035a884591afd0283fe69a53b49e552d2c1e744f
Jos Modding <josmodding@gmail.com>
2023-08-17 20:18:04 +0200

    V1.0.0.1

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,20 +1,20 @@
-ExtendedStrawTypes = {}
+ExtendedStrawCrops = {}
 local modDir = g_currentModDirectory
 
 --- Load data on map load.
-function ExtendedStrawTypes:loadMap()
+function ExtendedStrawCrops:loadMap()
     local modDescXML = loadXMLFile("modDesc", modDir .. "modDesc.xml")
     local filename = getXMLString(modDescXML, "modDesc.fruitTypes#filename")
     local xmlFile = loadXMLFile("modFruitTypes", modDir .. filename)
 
-    ExtendedStrawTypes:loadFruitTypeWindrow(xmlFile)
+    ExtendedStrawCrops:loadFruitTypeWindrow(xmlFile)
     delete(xmlFile)
     delete(modDescXML)
 end
 
 --- Loads fruitType windrow data from mod.
 -- @param table xmlFile
-function ExtendedStrawTypes:loadFruitTypeWindrow(xmlFile)
+function ExtendedStrawCrops:loadFruitTypeWindrow(xmlFile)
     if type(xmlFile) ~= "table" then
         xmlFile = XMLFile.wrap(xmlFile, FruitTypeManager.xmlSchema)
     end
@@ -23,16 +23,18 @@ function ExtendedStrawTypes:loadFruitTypeWindrow(xmlFile)
         local fruitTypeName = xmlFile:getValue(key .. "#name")
         local windrowName = xmlFile:getValue(key .. ".windrow#name")
         local litersPerSqm = xmlFile:getValue(key .. ".windrow#litersPerSqm")
-
         local fruitType = g_fruitTypeManager:getFruitTypeByName(fruitTypeName)
         local windrowType = g_fillTypeManager:getFillTypeByName(windrowName)
 
-        g_fruitTypeManager.fruitTypes[fruitType.index].hasWindrow = true
-        g_fruitTypeManager.fruitTypes[fruitType.index].windrowName = windrowType.name
-        g_fruitTypeManager.fruitTypes[fruitType.index].windrowLiterPerSqm = litersPerSqm
-        g_fruitTypeManager.fruitTypeIndexToWindrowFillTypeIndex[fruitType.index] = windrowType.index
-        g_fruitTypeManager.fillTypeIndexToFruitTypeIndex[windrowType.index] = fruitType.index
+        -- Skip if map adds windrow data.
+        if fruitType.hasWindrow == nil then
+            g_fruitTypeManager.fruitTypes[fruitType.index].hasWindrow = true
+            g_fruitTypeManager.fruitTypes[fruitType.index].windrowName = windrowType.name
+            g_fruitTypeManager.fruitTypes[fruitType.index].windrowLiterPerSqm = litersPerSqm
+            g_fruitTypeManager.fruitTypeIndexToWindrowFillTypeIndex[fruitType.index] = windrowType.index
+            g_fruitTypeManager.fillTypeIndexToFruitTypeIndex[windrowType.index] = fruitType.index
+        end
     end)
 end
 
-addModEventListener(ExtendedStrawTypes)
+addModEventListener(ExtendedStrawCrops)
